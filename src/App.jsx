9d05a1bc58ccab753322959ee84bb7c2f5c3fb86
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-06 21:54:29 +0100

    added Navbar and header componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React  from "react";
+import Header from "./components/Header";
+import './index.css'
+
 const App = () => {
   return (
-  <div>
-  </div>
+  <>
+    <Header/>
+  </>
   )
 }
 export default App;
