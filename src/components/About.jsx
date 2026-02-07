59e1f8d88c0ba5137b0dfa63ba370d2510f37a58
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-07 21:50:42 +0100

    About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,6 +43,10 @@ const About = () => {
                     </p>
                  </div>
                 </div>
+                    <p className='my-10 max-w-lg'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                 </p>
+                 <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
 
             </div>
         </div>
