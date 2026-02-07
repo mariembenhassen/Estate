d21aaf3fc74edf534df36bfc27ba1b72d4b960c7
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-08 00:59:44 +0100

    Contact section and email successfuly sent

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import './index.css'
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
+import Contact from "./components/Contact";
+  import { ToastContainer} from 'react-toastify';
 
 const App = () => {
   return (
   <div className="w-full overflow-hidden">
+    <ToastContainer/>
     <Header/>
     <About/>
     <Projects/>
     <Testimonails/>
+    <Contact/>
   </div>
   )
 }
