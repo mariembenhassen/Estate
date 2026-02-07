c9510649570cf70ebb49b59d5e1dea56f3d327c0
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-07 21:45:47 +0100

    show and hide Menu done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React  from "react";
 import Header from "./components/Header";
 import './index.css'
+import About from "./components/About";
 
 const App = () => {
   return (
   <div className="w-full overflow-hidden">
     <Header/>
+    <About/>
   </div>
   )
 }
